966b0c263287f10e5b3041e9cf07e1c9749a8fc7
alyssonramos <alyssonramos11@gmail.com>
2023-06-01 03:50:33 -0300

    Atualização geral

--- SITE-FORMULARIO/Telas/FormPreencher/formPreencher.js
+++ SITE-FORMULARIO/Telas/FormPreencher/formPreencher.js
@@ -19,3 +19,20 @@ function mostrarCampoTexto() {
     campoTexto.style.display = radio.checked ? "block" : "none";
   }
   
+  const getElement = (...queries) => document.querySelector(...queries);
+
+const button = getElement('.nav-link');
+const container = getElement('.modal-container');
+const modal = getElement('.modal');
+
+const activeModalClass = 'modal-show';
+
+const openModal = () => container.classList.add(activeModalClass);
+const closeModal = () => container.classList.remove(activeModalClass);
+
+button.addEventListener('click', openModal);
+container.addEventListener('click', (event) => {
+	if (modal.contains(event.target)) return;
+	
+	closeModal();
+});
--- SITE-FORMULARIO/Telas/forms/script.js
+++ SITE-FORMULARIO/Telas/forms/script.js
@@ -25,3 +25,21 @@ function adicionarFormulario(botao) {
 
   contador++; // Incrementa o contador para a próxima pergunta
 }
+
+const getElement = (...queries) => document.querySelector(...queries);
+
+const button = getElement('.nav-link');
+const container = getElement('.modal-container');
+const modal = getElement('.modal');
+
+const activeModalClass = 'modal-show';
+
+const openModal = () => container.classList.add(activeModalClass);
+const closeModal = () => container.classList.remove(activeModalClass);
+
+button.addEventListener('click', openModal);
+container.addEventListener('click', (event) => {
+	if (modal.contains(event.target)) return;
+	
+	closeModal();
+});
